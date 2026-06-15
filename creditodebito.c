b86d5b4a8796e82924e464723d7d733e71c02333
azul barra <azultaianab@gmail.com>
2026-06-14 21:38:25 -0300

    creacion de struct

--- creditodebito.c
+++ creditodebito.c
@@ -1,3 +1,22 @@
+//Laboratorio 3
+//Alumnas: Azul Barra y Alexia Insua
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <semaphore.h>
+
+typedef struct 
+{
+    sem_t semaforo;
+    double saldo;
+} memoriaCompartida;
+
+
+
 int main()
 {
     
